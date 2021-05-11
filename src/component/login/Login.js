b7763886b0e3e5af0e63f82903bb50aed7f052db
Abhishek <abhishekyadavfeb1498@gmail.com>
2021-05-11 11:06:42 +0530

    working on loginPage

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Login.css';
 
 const Login = () => {
+	const userName=(event)=>{
+		console.log('userName',event.target.value)
+	};
+	const userPassword=(event)=>{
+		console.log("userPassword",event.target.value)
+	}
   return (
     <div className='main_div'>
       <div className='login_box'>
         <h4>Login</h4>
 				<div className='credential_div'>
-				<input className='login_input' placeholder="Email ID" type='text'/>
-				<input className='login_input' placeholder='Password' type ='password'/>
+				<input className='login_input' placeholder="Email ID" type='text' onChange={userName}/>
+				<input className='login_input' placeholder='Password' type ='password' onChange={userPassword}/>
 				<span className='login_button'>Login</span>
 				</div>
       </div>
